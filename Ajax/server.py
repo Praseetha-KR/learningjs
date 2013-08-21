3983e7bf7f006e26978b376d0a9707f36710afde
Sharat M R <cosmosgenius@gmail.com>
2013-08-22 02:17:12 +0530

    Added header for CORS support

--- Ajax/server.py
+++ Ajax/server.py
@@ -4,10 +4,11 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(request):
         request.send_response(200);
         request.send_header("Content-type", "text/html")
+        request.send_header("Access-Control-Allow-Origin", "*")
         request.end_headers();
         request.wfile.write(random.randint(0,10**9));
 
-        
+
 if __name__ == '__main__':
     httpd = BaseHTTPServer.HTTPServer(('',8002),Handler)
     print ("Server Started");
