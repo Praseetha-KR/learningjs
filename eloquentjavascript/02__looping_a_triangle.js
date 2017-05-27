1ad8f3cd0bf95e833fc3f6c59f064ba4329d9011
Praseetha-KR <praseetha04@gmail.com>
2017-05-27 18:11:54 +0530

    Import validator in looping triangle

--- eloquentjavascript/02__looping_a_triangle.js
+++ eloquentjavascript/02__looping_a_triangle.js
@@ -1,15 +1,14 @@
+const validateExec = require('./validate');
+
 function displayTriangle(limit, char) {
     for (let i = 1; i <= limit; i++) {
         console.log(Array(i + 1).join(char || '#'));
     }
 }
 
-function validateArgAndExecute(fn) {
-    if (!arguments[1]) {
-        console.log('Provide a limit');
-        return;
-    }
-    fn(arguments[1], arguments[2]);
-}
-
-validateArgAndExecute(displayTriangle, process.argv[2], process.argv[3]);
+validateExec.call(
+    null,
+    displayTriangle,
+    parseInt(process.argv[2]),
+    process.argv[3]
+);
