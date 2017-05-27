f87b833a4b04939fc1fcb3677976da1834e3d0bc
Praseetha-KR <praseetha04@gmail.com>
2017-05-27 21:11:03 +0530

    Implements minimum & odd-even

---
+++ eloquentjavascript/03__minimum.js
@@ -0,0 +1,16 @@
+const validateExec = require('./validate');
+
+function min(num1,num2) {
+    return (num1 < num2) ? num1 : num2;
+}
+
+function displayMin(num1,num2) {
+    console.log(min(num1,num2));
+}
+
+validateExec.call(
+    null,
+    displayMin,
+    parseInt(process.argv[2]),
+    parseInt(process.argv[3])
+);
---
+++ eloquentjavascript/03__odd_or_even.js
@@ -0,0 +1,18 @@
+const validateExec = require('./validate');
+
+function oddOrEven(num) {
+    const SUBTRACTOR = (num < 0) ? -2 : 2;
+    if (num === 0) return 'even';
+    if (num === 1) return 'odd';
+    return oddOrEven(num - SUBTRACTOR);
+}
+
+function displayOddOrEven(num) {
+    console.log(oddOrEven(num));
+}
+
+validateExec.call(
+    null,
+    displayOddOrEven,
+    parseInt(process.argv[2])
+)
