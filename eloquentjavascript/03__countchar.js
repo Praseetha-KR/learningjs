d34c5d753f995844a72f9d943a4617becfa11b09
Praseetha-KR <praseetha04@gmail.com>
2017-05-27 21:21:59 +0530

    Implements countchar

---
+++ eloquentjavascript/03__countchar.js
@@ -0,0 +1,17 @@
+const validateExec = require('./validate');
+
+function countChar(str, char) {
+    const filtered = str.split('').filter(v => v === char);
+    return filtered.length;
+}
+
+function displayCountChar(str, char) {
+    console.log(countChar(str, char));
+}
+
+validateExec.call(
+    null,
+    displayCountChar,
+    process.argv[2],
+    process.argv[3]
+);
