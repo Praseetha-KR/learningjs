95918299fb6c5cbcfab19fd05d77fe01cdce0c5c
Praseetha-KR <praseetha04@gmail.com>
2017-05-27 18:12:31 +0530

    Implements fizzbuzz

---
+++ eloquentjavascript/02__fizzbuzz.js
@@ -0,0 +1,27 @@
+const validateExec = require('./validate');
+
+function fizzbuzz(start, end, step=1) {
+    for (let i = start; i <= end; i += step) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log('FizzBuzz');
+            continue;
+        }
+        if (i % 3 == 0) {
+            console.log('Fizz');
+            continue;
+        }
+        if (i % 5 == 0) {
+            console.log('Buzz');
+            continue;
+        }
+        console.log(i);
+    }
+}
+
+validateExec.call(
+    null,
+    fizzbuzz,
+    parseInt(process.argv[2]),
+    parseInt(process.argv[3]),
+    parseInt(process.argv[4])
+);
