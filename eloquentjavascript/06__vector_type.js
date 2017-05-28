9c71e7393c081d6b800b91d795349be1e7b8de7f
Praseetha-KR <praseetha04@gmail.com>
2017-05-28 23:12:35 +0530

    Implements vector type

---
+++ eloquentjavascript/06__vector_type.js
@@ -0,0 +1,28 @@
+class Vector {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    plus(v) {
+        return new Vector(
+            this.x + v.x,
+            this.y + v.y
+        );
+    }
+
+    minus(v) {
+        return new Vector(
+            this.x - v.x,
+            this.y - v.y
+        );
+    }
+
+    get length() {
+        return Math.sqrt(this.x**2 + this.y**2);
+    }
+}
+
+console.log(new Vector(1, 2).plus(new Vector(2, 3)));
+console.log(new Vector(1, 2).minus(new Vector(2, 3)));
+console.log(new Vector(3, 4).length);
