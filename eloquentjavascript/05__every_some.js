16b3a43c971dbb79af17abe0733725c9855c7c47
Praseetha-KR <praseetha04@gmail.com>
2017-05-28 20:23:39 +0530

    Implements some & every

---
+++ eloquentjavascript/05__every_some.js
@@ -0,0 +1,13 @@
+function some(arr, predicate) {
+    return !!(arr.filter(v => predicate(v)).length);
+}
+
+function every(arr, predicate) {
+    return arr.length === arr.filter(v => predicate(v)).length;
+}
+
+
+console.log(every([NaN, NaN, NaN], isNaN));
+console.log(every([NaN, NaN, 4], isNaN));
+console.log(some([NaN, 3, 4], isNaN));
+console.log(some([2, 3, 4], isNaN));
